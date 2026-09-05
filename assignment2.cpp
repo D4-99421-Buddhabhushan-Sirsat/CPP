e7d97014335fa59529238b83ab4ecf812311feff
Buddhabhushan Sirsat <bhushan@Buddhabhushans-MacBook-Air.local>
2026-09-05 15:06:54 +0530

    question 1 of assignment 2 pushed

---
+++ assignment2.cpp
@@ -0,0 +1,83 @@
+// Q1) Menu-driven volume calculation using constructor overloading.
+
+#include <iostream>
+
+using namespace std;
+
+class Box {
+private:
+	int length;
+	int breadth;
+	int height;
+
+public:
+	Box() : length(1), breadth(1), height(1) {}
+
+	Box(int side) : length(side), breadth(side), height(side) {}
+
+	Box(int boxLength, int boxBreadth, int boxHeight){
+        this->length = boxLength;
+        this->breadth = boxBreadth;
+        this->height = boxHeight;
+    }
+
+    // Box(int boxLength, int boxBreadth, int boxHeight) : length(boxLength), breadth(boxBreadth), height(boxHeight)
+    // {
+
+    // }
+
+	int calculateVolume() {
+		return length * breadth * height;
+	}
+};
+
+int main() {
+	int choice;
+
+	do {
+		cout << "\n--- Box Volume Menu ---\n"
+			 << "1. Calculate Volume with default values\n"
+			 << "2. Calculate Volume with length, breadth and height with same value\n"
+			 << "3. Calculate Volume with different length, breadth and height values\n"
+			 << "0. Exit\n"
+			 << "Enter your choice: ";
+		cin >> choice;
+
+		switch (choice) {
+			case 1: {
+				Box box;
+				cout << "Volume: " << box.calculateVolume() << '\n';
+				break;
+			}
+
+			case 2: {
+				int side;
+				cout << "Enter the common value: ";
+				cin >> side;
+				Box box(side);
+				cout << "Volume: " << box.calculateVolume() << '\n';
+				break;
+			}
+
+			case 3: {
+				int length;
+				int breadth;
+				int height;
+				cout << "Enter length, breadth and height: ";
+				cin >> length >> breadth >> height;
+				Box box(length, breadth, height);
+				cout << "Volume: " << box.calculateVolume() << '\n';
+				break;
+			}
+
+			case 0:
+				cout << "Exiting program.\n";
+				break;
+
+			default:
+				cout << "Invalid choice.\n";
+		}
+	} while (choice != 0);
+
+	return 0;
+}
